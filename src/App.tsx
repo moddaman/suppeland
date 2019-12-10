a99d55022fceef76da3c39c3c31c27367e665f98
moddaman <ovrebo90@gmail.com>
2019-12-10 18:29:12 +0100

    make entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,24 @@
 import React from "react";
 import "./App.css";
-import stillImage from "./images/IMG_0682.jpg";
-import videos from "./images/IMG_0682.mp4";
-import linnBilde from "./images/linn.jpg";
-import linnVideo from "./images/linn.mp4";
-import jonasBilde from "./images/jonas.jpg";
-import jonasVideo from "./images/jonas.mp4";
-import mortenBilde from "./images/mortenMarked.jpg";
-import mortenVideo from "./images/mortenMarked.mp4";
-import strandBilde from "./images/strand.jpg";
-import strandVideo from "./images/strand.mp4";
-// import victorBilde from './images/victorPaaBerget.jpg';
-// import victorVideo from './images/victorPaaBerget.mp4';
-import LivePhoto from "./LivePhoto/LivePhoto";
-import FunnyText from "./FunnyText/FunnyText";
+import FunnyText from "./Common/FunnyText/FunnyText";
+import { AboutLivePhoto } from "./Entries/AboutLivePhoto";
 
 // import * as LivePhotosKit from 'livephotoskit';
 
 const App: React.FC = () => {
-  console.log(stillImage);
   return (
     <div className="App">
       <header className="App-header">
-        <FunnyText text={"Altsa Ogsa"} />
+        <FunnyText text={"RANGO"} />
       </header>
-      <div className="photo-container">
+      <AboutLivePhoto />
+      {/* <div className="photo-container">
         <LivePhoto imagePath={jonasBilde} videoPath={jonasVideo} />
         <LivePhoto imagePath={stillImage} videoPath={videos} />
         <LivePhoto imagePath={linnBilde} videoPath={linnVideo} />
         <LivePhoto imagePath={mortenBilde} videoPath={mortenVideo} />
         <LivePhoto imagePath={strandBilde} videoPath={strandVideo} />
-      </div>
+      </div> */}
 
       {/* <div className='container'>
         <div className='cool'>
--- src/Common/LivePhoto/LivePhoto.tsx
+++ src/Common/LivePhoto/LivePhoto.tsx
@@ -1,31 +1,29 @@
 import React, { useRef, useEffect, useState } from "react";
 import * as LivePhotosKit from "livephotoskit";
 import style from "./LivePhoto.module.scss";
+import { PlaybackStyleLiteral } from "livephotoskit";
 
 interface IProps {
   imagePath: string;
   videoPath: string;
-  width?: number;
-  height?: number;
+  playbackStyle?: PlaybackStyleLiteral;
 }
 
 const LivePhoto = ({
   imagePath,
   videoPath,
-  width = 320,
-  height = 320
+  playbackStyle = LivePhotosKit.PlaybackStyle.LOOP
 }: IProps) => {
   const textInput = useRef<HTMLInputElement>(null);
-
   const [player, setPlayer] = useState<LivePhotosKit.Player>();
 
   useEffect(() => {
     if (textInput && textInput.current) {
-      const p = LivePhotosKit.Player(textInput.current);
-      setPlayer(p);
+      const htmlPlayer = LivePhotosKit.Player(textInput.current);
+      setPlayer(htmlPlayer);
       if (player) {
         player.showsNativeControls = false;
-        player.playbackStyle = LivePhotosKit.PlaybackStyle.LOOP;
+        player.playbackStyle = playbackStyle;
       }
     }
   });
@@ -46,6 +44,8 @@ const LivePhoto = ({
     <div
       className={style.photo}
       onMouseEnter={playLoop}
+      onTouchStart={playLoop}
+      onTouchEnd={playStop}
       onMouseLeave={playStop}
       ref={textInput}
       data-live-photo
