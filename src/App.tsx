809e3b6018324870112e73b732027eafce0ecadb
moddaman <ovrebo90@gmail.com>
2020-02-06 19:40:44 +0100

    dsa

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,6 @@ const App: React.FC = () => {
         <FunnyText text={"RANGO"} />
       </header>
       <AboutLivePhoto />
-      {/* <div className="photo-container">
-        <LivePhoto imagePath={jonasBilde} videoPath={jonasVideo} />
-        <LivePhoto imagePath={stillImage} videoPath={videos} />
-        <LivePhoto imagePath={linnBilde} videoPath={linnVideo} />
-        <LivePhoto imagePath={mortenBilde} videoPath={mortenVideo} />
-        <LivePhoto imagePath={strandBilde} videoPath={strandVideo} />
-      </div> */}
-
-      {/* <div className='container'>
-        <div className='cool'>
-          <h1>Denne siden var bra!</h1>
-        </div>
-        <div className='lame'>
-          <h1>Denne siden var Dårlig!</h1>
-        </div>
-      </div> */}
     </div>
   );
 };
